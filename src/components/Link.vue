10e4b1b9e1fcb28c643e9646e33be98155d09740
Raul García Martín <raulgm83@gmail.com>
2022-09-12 08:22:21 +0200

    Link component transition

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -22,7 +22,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .link {
   display: inline-block;
   background-color: var(--secondary-color);
@@ -30,5 +30,11 @@ export default {
   font-family: var(--font-text-bold);
   margin-top: 1rem;
   padding: 1rem 2rem;
+  border-radius: 5px;
+  box-shadow: 0 0 7px 0 var(--secondary-color);
+  transition: box-shadow .4s ease-in-out;
+  &:hover {
+    box-shadow: 0 0 8px 3px var(--secondary-color);
+  }
 }
 </style>
