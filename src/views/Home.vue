cc213ffb1cbe34aade6ad6677d23b5a6b1a11730
Raul García Martín <raulgm83@gmail.com>
2022-10-02 16:48:18 +0200

    main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,52 @@
 <template>
-      <h1 class="title">HELLO WORLD</h1>
     <section class="main">
-      <article class="main__option main__developer">
-        <router-link to="/developer-side">
-          <h1>Developer Side</h1>
-        </router-link>
-      </article>
-      <article class="main__option main__designer">
-        <router-link to="/designer-side">
-          <h1>Designer Side</h1>
-        </router-link>
-      </article>
+      <div class="main__background">
+        <Image
+          class="main__option__image"
+          file="portfolio/Raul/DeveloperSideDefault.png"
+          altTitle="Raúl Developer side por defecto"
+        />
+        <Image
+          class="main__option__image"
+          file="portfolio/Raul/DesignerSideDefault.png"
+          altTitle="Raúl Designer side por defecto"
+        />
+      </div>
+      
+      <!--
+        <div class="main__options">
+
+      </div>
+        <article class="main__option main__developer">
+          <router-link to="/developer-side">
+            <Image
+              class="main__option__image"
+              file="portfolio/Raul/DeveloperSideDefault.png"
+              altTitle="Raúl Developer side por defecto"
+            />
+          </router-link>
+        </article>
+        <article class="main__option main__designer">
+          <router-link to="/designer-side">
+            <Image
+              class="main__option__image"
+              file="portfolio/Raul/DesignerSideDefault.png"
+              altTitle="Raúl Designer side por defecto"
+            />
+          </router-link>
+        </article>
+      -->
+      
     </section>
 </template>
 
 <script>
+import Image from '@/components/Image.vue';
+
 export default {
   name: 'Home',
   components: {
+    Image
   }
 }
 </script>
@@ -25,25 +54,15 @@ export default {
 :root {
   --main-border: 3px solid black;
 }
-.title {
-  position: absolute;
-}
-.main {
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-}
-.main__option {
-  flex: 1;
-}
-.main__option a{
-  display: block;
-  height: 100%;
-}
-.main__developer {
-  border-right: var(--main-border);
-}
 .main__designer {
   border-left: var(--main-border);
+}
+.main {
+  height: 100%;
+  width: 100%;
+  background: pink;
+  &__background {
+    display: flex;
+  }
 }
 </style>
