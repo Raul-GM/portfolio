03527a851484a5d212c11da7489c80d83a186ccc
Raul García Martín <raulgm83@gmail.com>
2022-10-11 08:42:15 +0200

    First page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,37 @@
 <template>
     <section class="main">
       <div class="main__background">
-        <Image
-          class="main__option__image"
-          file="portfolio/Raul/DeveloperSideDefault.png"
-          altTitle="Raúl Developer side por defecto"
-        />
-        <Image
-          class="main__option__image"
-          file="portfolio/Raul/DesignerSideDefault.png"
-          altTitle="Raúl Designer side por defecto"
-        />
+        <div>
+          <Image
+            class="main__option__image"
+            file="portfolio/Raul/DeveloperSideDefault.png"
+            altTitle="Raúl Developer side por defecto"
+          />
+        </div>
+        <div>
+          <Image
+            class="main__option__image"
+            file="portfolio/Raul/DesignerSideDefault.png"
+            altTitle="Raúl Designer side por defecto"
+          />
+        </div>
       </div>
-      
-      <!--
-        <div class="main__options">
-
+      <div class="main__options">
+        <router-link to="/developer-side">
+          <Image
+            class="main__option__image"
+            file="portfolio/Raul/DeveloperSideHover.png"
+            altTitle="Raúl Developer side por defecto"
+          />
+        </router-link>
+        <router-link to="/designer-side">
+          <Image
+            class="main__option__image"
+            file="portfolio/Raul/DesignerSideHover.png"
+            altTitle="Raúl Designer side por defecto"
+          />
+        </router-link>
       </div>
-        <article class="main__option main__developer">
-          <router-link to="/developer-side">
-            <Image
-              class="main__option__image"
-              file="portfolio/Raul/DeveloperSideDefault.png"
-              altTitle="Raúl Developer side por defecto"
-            />
-          </router-link>
-        </article>
-        <article class="main__option main__designer">
-          <router-link to="/designer-side">
-            <Image
-              class="main__option__image"
-              file="portfolio/Raul/DesignerSideDefault.png"
-              altTitle="Raúl Designer side por defecto"
-            />
-          </router-link>
-        </article>
-      -->
       
     </section>
 </template>
@@ -58,11 +54,36 @@ export default {
   border-left: var(--main-border);
 }
 .main {
-  height: 100%;
+  height: 100vh;
   width: 100%;
   background: pink;
-  &__background {
-    display: flex;
+  overflow: hidden;
+  &__background,
+  &__options {
+    height: 100%;
+    display: grid;
+    width: 100%;
+    top: 0;
+    position: absolute;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    div:first-child,
+    a:first-child {
+      justify-self: flex-end;
+    }
+    img {
+      width: auto;
+      height: 100vh;
+    }
+  }
+  &__options {
+    img {
+      opacity: 0%;
+      transition: .5s opacity ease-in;
+      &:hover {
+        opacity: 100%;
+      }
+    }
   }
 }
 </style>
