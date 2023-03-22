84bfcbed860fe68b3d2347d240fd85d6261721d3
Raul García Martín <raulgm83@gmail.com>
2023-03-22 15:59:59 +0100

    POC Optimized Images

---
+++ src/components/OptimizedImage.vue
@@ -0,0 +1,32 @@
+<template>
+    <picture>
+      <source :srcset="getLargeImage" media="(min-width: 1024px)">
+      <source :srcset="getMediumImage" media="(min-width: 640px)">
+      <img class="image" :src="getSmallImage" :alt="altTitle">
+    </picture>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      image: {
+        type: String
+      },
+      altTitle: {
+        type: String,
+        default: 'Image'
+      }
+    },
+    computed: {
+        getLargeImage() {
+            return require(`../assets/images/${this.image}_1024.png`)
+        },
+        getMediumImage() {
+            return require(`../assets/images/${this.image}_640.png`)
+        },
+        getSmallImage() {
+            return require(`../assets/images/${this.image}_320.png`)
+        }
+    }
+  }
+  </script>
--- src/views/designerSide/portfolio/MalasMujeres.vue
+++ src/views/designerSide/portfolio/MalasMujeres.vue
@@ -7,7 +7,8 @@
                 <p>Gráfica publicitaria / Póster del libro de María Hesse, "Malas Mujeres". Trabajo para la asignatura de diseño gráfico consistente en hacer una gráfica publicitaria en formato A4 sobre un producto a nuestra elección usando sólamente componentes hechos con papel, cartulina o derivados y sin realizar ningún retoque via Photoshop más que ajustes de luces, sombras, tono, etc.</p>
             </div>
             <div class="portfolio-project-container__images full">
-                <Image file="portfolio/MalasMujeres/Malas_Mujeres.png" altTitle="Gráfica final de Malas Mujeres" />
+                <OptimizedImage image="portfolio/MalasMujeres/Malas_Mujeres"
+                    altTitle="Gráfica final de Malas Mujeres" />
                 <Image file="portfolio/MalasMujeres/Malas_Mujeres_Poster_2.png" altTitle="Póster de la gráfica en marco apoyado en la pared" />
                 <Image file="portfolio/MalasMujeres/Malas_Mujeres_Poster.png" altTitle="Póster de la gráfica colgado en la pared" />
             </div>
@@ -19,12 +20,13 @@
 import Navbar from '@/components/Navbar.vue';
 import Footer from '@/components/Footer.vue';
 import Image from '@/components/Image.vue';
-
+import OptimizedImage from '@/components/OptimizedImage.vue';
 export default {
     components: {
         Navbar,
         Footer,
-        Image
+        Image,
+        OptimizedImage
     }
 }
 </script>
