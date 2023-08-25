d2be3d4e4537d648f0c4d0f218e545a2d57dceff
Raul García Martín <raulgm83@gmail.com>
2023-08-25 13:07:52 +0200

    Set new colors. Changed to PhaseII the new elements

---
+++ src/components/phaseII/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+    <footer class="footer">
+        <div class="footer-container">
+            <span>
+                &copy; {{ getYear() }} Raúl García Martín
+            </span>
+            <span>
+                Redes sociales
+            </span>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    methods: {
+        getYear() {
+            return new Date().getFullYear()
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.footer {
+    background-color: var(--darkest-color);
+    color: var(--secondary-text-color);
+    height: var(--footer-height);
+    font-size: .9rem;
+    &-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 100%;
+        max-width: 1024px;
+        padding: 0 5rem;
+        margin: auto;
+    }
+}
+</style>
---
+++ src/views/phaseII/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <Navbar sideType="designer" />
+    <div class="main-container">
+        
+    </div>
+    <Footer />
+</template>
+
+<script>
+import Navbar from '@/components/phaseII/Navbar.vue';
+import Footer from '@/components/phaseII/Footer.vue';
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+  }
+}
+</script>
+
+<style>
+
+</style>
