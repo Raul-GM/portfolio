6383902fb909a0e0c9203fc6e09a8f0a0861dfd1
Raul García Martín <raulgm83@gmail.com>
2023-09-03 10:17:43 +0200

    project name changed. Some fixes in About Me

--- src/views/AboutMeComponent.vue
+++ src/views/AboutMeComponent.vue
@@ -4,7 +4,7 @@
         <div class="about-me-detail-container">
           <ImageComponent file="portfolio/Raul/Vectorizado-assets/Vectorizado.png"/>
           <div>
-            <p><b>¡Hola!</b> Soy Raúl García. Soy desarrollador front y estudiante de un grado de Diseño Digital en la Universidad Internacional de La Rioja (UNIR).</p>
+            <p><b>¡Hola!</b> Soy Raúl García, estudiante de Grado de Diseño Digital en la Universidad Internacional de La Rioja (UNIR) y desarrollador front.</p>
             <p>Aunque mi carrera profesional comenzó como programador, siempre he tenido esa chispa artística y creativa latente. Trabajar en estrecha colaboración con talentosos diseñadores de UI/UX a lo largo de los años me inspiró a entender más profundamente las decisiones detrás de sus diseños. Cada vez que adquiría conocimientos de sus perspectivas, mi curiosidad crecía y me sumergía en la exploración de este fascinante mundo del diseño.</p>
             <p>En el inusual año de 2020, en medio de la agitación de la pandemia, tomé una decisión arriesgada: dar rienda suelta a mi pasión y sumergirme en el Grado de Diseño Digital. Desde entonces, he estado disfrutando cada momento como nunca antes. Aprendo nuevas habilidades y no puedo evitar aplicarlas cada vez que surge una oportunidad.</p>
             <p>Por otro lado, decidí desempolvar también mis viejos lápices y empezar a dibujar de nuevo, una afición que había dejado a un lado desde la infancia. Aunque no es una habilidad fundamental para el diseño digital, descubrí que complementa maravillosamente mi enfoque creativo. Más que eso, es una vía para liberar mi mente y encontrar relajación en un mundo cada vez más vertiginoso.</p>
